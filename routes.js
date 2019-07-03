1fd1c31189f0a21cd3e5771890ee65ba69fdc6f0
Carlos Mario Herrera <cmarioherrera@gmail.com>
2019-07-03 14:23:20 -0500

    Create the products' route

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@
  * Main application routes
  */
 const helloworld = require('./api/helloworld');
+const product = require('./api/product');
 
 module.exports = (app) => {
   app.use('/api/helloworld', helloworld);
+  app.use('/api/product', product);
 };
